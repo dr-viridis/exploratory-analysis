8f9e30650af3052b24655244e46f5077700955ef
Don Green <dr-viridis@users.noreply.github.com>
2015-12-13 17:27:03 -0600

    fixed some formating for plot3

--- plot3.R
+++ plot3.R
@@ -6,12 +6,14 @@ png("./plot3.png")
 
 y_limits <- c(0,0.035) # "cheating" by not calculating this on the fly
 
-plot(as.numeric(my_data$Sub_metering_1)/1000, type = "s", ylim = y_limits)
+par(pin = c(3,3)) # not sure if these are the "right" numbers but my 
+			# first plots were awfully small
+plot(as.numeric(my_data$Sub_metering_1)/1000, type = "s", ylim = y_limits, ylab = "Energy Sub Metering", xlab = " ")
 par(col = "red")
 lines(as.numeric(my_data$Sub_metering_2)/1000, type = "s")
 par(col = "blue")
 lines(as.numeric(my_data$Sub_metering_3)/1000, type = "s")
 
-legend("topright", c("sub_metering_1","sub_metering_2","sub_metering_3"), lty = 1, col = 1:3)
+legend("topright", c("sub_metering_1","sub_metering_2","sub_metering_3"), lty = 1, col = c("black","red","blue"))
 
 dev.off()
